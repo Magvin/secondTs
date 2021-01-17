f1184309c64ec08fc1084770629dce09f1720488
Magvin <zans.litinskis@gmail.com>
2021-01-17 16:17:21 +0000

    CharactersCollection

--- build/index.js
+++ build/index.js
@@ -2,7 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
 var NumbersCollection_1 = require("./NumbersCollection");
+var CharactersCollection_1 = require("./CharactersCollection");
 var numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
+var charactersCollection = new CharactersCollection_1.CharastersCollection('somEAnother');
 var sorter = new Sorter_1.Sorter(numbersCollection);
+var charactersSorter = new Sorter_1.Sorter(charactersCollection);
 sorter.sort();
+charactersSorter.sort();
 console.log(numbersCollection.data);
+console.log(charactersCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharastersCollection } from './CharactersCollection';
 const numbersCollection = new NumbersCollection([10,3,-5,0])
+const charactersCollection = new CharastersCollection('somEAnother');
 const sorter = new Sorter(numbersCollection);
+const charactersSorter = new Sorter(charactersCollection);
 sorter.sort();
+charactersSorter.sort();
 console.log(numbersCollection.data)
+console.log(charactersCollection.data)
